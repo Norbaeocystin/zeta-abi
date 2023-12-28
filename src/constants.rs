8e8c1adf204b1df7f4451bbaa532218149c98a93
Norbaeocystin <baranrastislav@gmail.com>
2023-12-28 13:47:41 -0300

    Update constants.rs
    bonk

--- src/constants.rs
+++ src/constants.rs
@@ -3,6 +3,6 @@ pub const NATIVE_PRECISION_DENOMINATOR: u128 = 100_000_000; // 100%
 pub const BID_ORDERS_INDEX: usize = 0;
 pub const ASK_ORDERS_INDEX: usize = 1;
 pub const ACTIVE_PERP_MARKETS: usize = 11;
-pub const UNUSED_PERP_MARKETS: usize = 16;
+pub const UNUSED_PERP_MARKETS: usize = 14;
 pub const ACCOUNT_NAME_MAX_LEN: usize = 10;
 pub const MAX_CROSS_MARGIN_ACCOUNTS: usize = 25;
